f7e2a85eb305fe2d008b1f3bfe4f8165c8045239
oliv-bernier <olivbernier@gmail.com>
2023-12-04 19:27:14 +0100

    day 3 part two solved

--- src/day3/solution.ts
+++ src/day3/solution.ts
@@ -55,12 +55,10 @@ console.log('Solution of Part one =>', partOneSolution)
 // *** PART TWO ***
 // ****************
 
-// TODO not finished yet
-
 const EXTRACT_ASTERISK_REGEX = /\*/g
 
-const browseRightForNumber = (input: string, startIndex: number) => {
-  const match = input.slice(startIndex + 1).match(EXTRACT_NUMBERS_REGEX)
+const browseRightForNumber = (input: string, startIndex: number, lineIndex: number) => {
+  const match = input.slice(startIndex).match(EXTRACT_NUMBERS_REGEX)
   return match ? match[0] : ''
 }
 
@@ -74,22 +72,16 @@ const browseLeftForNumber = (input: string, startIndex: number) => {
   return input.slice(endIndex + 1, startIndex + 1)
 }
 
-console.log('test right =>', browseRightForNumber('..89526..15..15', 3))
-
-console.log('test left =>', browseLeftForNumber('..89526..15..15', 4))
-
-// TODO redo this method
-const getWholeNumber = (input: string, startIndex: number): number => {
-  const rightResult = browseRightForNumber(input, startIndex)
+const getWholeNumber = (input: string, startIndex: number, lineIndex: number): number => {
+  const rightResult = browseRightForNumber(input, startIndex, lineIndex)
   const leftResult = browseLeftForNumber(input, startIndex)
 
   const number = leftResult + rightResult.slice(1)
 
-  console.log('number =>', number)
   return parseInt(number, 10)
 }
 
-const extractNumbersStartIndex = (input: string) => {
+const extractNumbersStartIndex = (input: string, lineIndex: number) => {
   const indexes = []
   let match
 
@@ -113,45 +105,39 @@ const getGearRatios = (engineSchematic: string[]): number[] => {
       const previousLine = engineSchematic[lineIndex - 1]
       const nextLine = engineSchematic[lineIndex + 1]
 
-      const leftNumber = EXTRACT_NUMBERS_REGEX.test(line[startIndex])
-        ? parseInt(browseLeftForNumber(line, startIndex), 10)
-        : null
-      const rightNumber = EXTRACT_NUMBERS_REGEX.test(line[endIndex])
-        ? parseInt(browseRightForNumber(line, endIndex), 10)
-        : null
+      const leftNumber = /\d+/.test(line[startIndex]) ? browseLeftForNumber(line, startIndex) : null
+      const rightNumber = /\d+/.test(line[endIndex]) ? browseRightForNumber(line, match.index, lineIndex) : null
 
       const numbersAboveStartIndexes = previousLine
-        ? extractNumbersStartIndex(previousLine.substring(startIndex, match.index + 1))
+        ? extractNumbersStartIndex(previousLine.substring(startIndex, match.index + 2), lineIndex)
         : []
       const numbersBelowStartIndexes = nextLine
-        ? extractNumbersStartIndex(nextLine.substring(startIndex, match.index + 1))
+        ? extractNumbersStartIndex(nextLine.substring(startIndex, match.index + 2), lineIndex)
         : []
 
       const numbersAbove: number[] = []
       const numbersBelow: number[] = []
 
       numbersAboveStartIndexes.forEach((numbersAboveIndex) => {
-        const numberAbove = getWholeNumber(previousLine, startIndex + numbersAboveIndex)
+        const numberAbove = getWholeNumber(previousLine, startIndex + numbersAboveIndex, lineIndex)
 
         numbersAbove.push(numberAbove)
       })
 
       numbersBelowStartIndexes.forEach((numbersBelowIndex) => {
-        const numberBelow = getWholeNumber(nextLine, startIndex + numbersBelowIndex)
+        const numberBelow = getWholeNumber(nextLine, startIndex + numbersBelowIndex, lineIndex)
 
         numbersBelow.push(numberBelow)
       })
 
       const foundPartNumbers: number[] = []
 
-      console.log(numbersAboveStartIndexes, numbersBelowStartIndexes)
-
       if (leftNumber) {
-        foundPartNumbers.push(leftNumber)
+        foundPartNumbers.push(parseInt(leftNumber, 10))
       }
 
       if (rightNumber) {
-        foundPartNumbers.push(rightNumber)
+        foundPartNumbers.push(parseInt(rightNumber, 10))
       }
 
       if (numbersAbove.length > 0) {
@@ -159,11 +145,11 @@ const getGearRatios = (engineSchematic: string[]): number[] => {
       }
 
       if (numbersBelow.length > 0) {
-        foundPartNumbers.push(...numbersAbove)
+        foundPartNumbers.push(...numbersBelow)
       }
 
       if (foundPartNumbers.length === 2) {
-        const gearRatio = foundPartNumbers.reduce((acc, partNumber) => acc * partNumber, 1)
+        const gearRatio = foundPartNumbers[0] * foundPartNumbers[1]
 
         gearRatios.push(gearRatio)
       }
